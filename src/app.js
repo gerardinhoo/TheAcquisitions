db7390863bd3e9834256a65833bdf1daa49d0975
Gerard <gerard.eklu@yahoo.fr>
2025-11-03 17:50:36 -0500

    Initial commit: Project structure, Initial setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from Devops Project Acquisitions')
+})
+
+
+export default app;
+
