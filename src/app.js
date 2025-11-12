32af20eb0f6dda481c96e3f4bde351d86129f4e4
Gerard <gerard.eklu@yahoo.fr>
2025-11-12 02:06:41 -0500

    Authentication - sign up: folder structure for routes, controllers, services, utils, validations

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import logger from '#config/logger.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
@@ -13,11 +14,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Hello from The Acquisitions');
   res.status(200).send('Hello from Devops Project Acquisitions');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'The Acquisitions API is running' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
--- src/config/logger.js
+++ src/config/logger.js
@@ -2,11 +2,11 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.combine((
-    winston.format.timestamp(),
-    winston.format.errors({stack: true}),
-    winston.format.json()
-  )),
+  format: winston.format.combine(
+    (winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json())
+  ),
   defaultMeta: { service: 'user-service' },
   transports: [
     new winston.transports.File({ filename: 'logs/error.lg', level: 'error' }),
@@ -14,15 +14,15 @@ const logger = winston.createLogger({
   ],
 });
 
-
 if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
-  }));
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
 }
-
 
 export default logger;
