3b82de8d36fd5dcfd478a12c425c11fbf09f2a7c
Gerard <gerard.eklu@yahoo.fr>
2025-11-16 13:09:16 -0500

    Implement security: Bot detection. Rate limiting

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import logger from '#config/logger.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security-middleware.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(securityMiddleware);
 
 app.use(
   morgan('combined', {
