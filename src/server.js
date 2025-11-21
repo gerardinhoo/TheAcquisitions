b0ac3a937fd727d421fa926163853f83511079b5
Gerard <gerard.eklu@yahoo.fr>
2025-11-20 22:37:04 -0500

    Fix linting and formatting

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
-const HOST = "0.0.0.0";
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log(`App listening on Port http://localhost:${PORT}`);
 });
-
-
